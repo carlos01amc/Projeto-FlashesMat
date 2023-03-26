e50d0791b4e1950b781b813cdf6573ad0244aa7c
Carlos Costa <carlosandremachadocosta@gmail.com>
2023-03-26 23:57:05 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ conn = sqlite3.connect('database.db')
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-app.config["UPLOAD_FOLDER"] = r'C:\Users\carlo\OneDrive\Ambiente de Trabalho\UM\3ano2s\Projeto\static\upload'
+app.config["UPLOAD_FOLDER"] = r'C:\Users\carlo\Desktop\UM\3ano2s\Projeto\static\upload'
 
 
 class LogForm(FlaskForm):
@@ -46,19 +46,19 @@ def login():
         else:
             return 'E-mail não encontrado!'
     else:
-        return render_template('login.html',can_edit=False)
+        return render_template('login.html', can_edit=False)
 
 
 @app.route('/logout/', methods=['GET', 'POST'])
 def logout():
     session.pop('email', None)
     session.pop('tipo', None)
-    return redirect(url_for('home',can_edit=False))
+    return redirect(url_for('home', can_edit=False))
 
 
 @app.route("/")
 def home():
-    return render_template("index.html",can_edit=False)
+    return render_template("index.html", can_edit=False)
 
 
 @app.route("/menu")
@@ -68,17 +68,17 @@ def menu():
     c.execute("SELECT * FROM posts")
     posts = c.fetchall()
     conn.close()
-    return render_template("menu.html",can_edit=False, posts=posts)
+    return render_template("menu.html", can_edit=False, posts=posts)
 
 
 @app.route("/criadores/")
 def criadores():
-    return render_template("creatores.html",can_edit=False)
+    return render_template("creatores.html", can_edit=False)
 
 
 @app.route("/sabermais/")
 def sabe():
-    return render_template("sabermais.html",can_edit=False)
+    return render_template("sabermais.html", can_edit=False)
 
 
 @app.route("/new-post", methods=["GET", "POST"])
@@ -104,7 +104,7 @@ def new_post():
         conn.commit()
         conn.close()
         return redirect("/")
-    return render_template("new_post.html",can_edit=False)
+    return render_template("new_post.html", can_edit=False)
 
 
 @app.route("/post/<int:post_id>")
@@ -119,14 +119,14 @@ def post(post_id):
     if 'email' in session and session['tipo'] == 'admin':
         can_edit = True
 
-    return render_template("post.html", post=post,can_edit=can_edit)
+    return render_template("post.html", post=post, can_edit=can_edit)
 
 
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 def edit_post(post_id):
     if 'email' not in session or session['tipo'] != 'admin':
         return 'Acesso negado!'
-    
+
     can_edit = False
     if 'email' in session and session['tipo'] == 'admin':
         can_edit = True
@@ -152,13 +152,35 @@ def edit_post(post_id):
         conn.commit()
         conn.close()
 
-
-
         return redirect(url_for("post", post_id=post_id))
 
     conn.close()
-    
+
     return render_template("edit_post.html", post=post, can_edit=can_edit)
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute('SELECT * FROM usuarios WHERE email=?', (email,))
+        user = c.fetchone()
+        if user:
+            return 'Este e-mail já foi registrado'
+
+        hash_senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+        c.execute('INSERT INTO usuarios (email, senha) VALUES (?, ?)',
+                  (email, hash_senha))
+        conn.commit()
+        conn.close()
+
+        return render_template('index.html')
+    else:
+        return render_template('register.html')
 
 
 if __name__ == "__main__":
